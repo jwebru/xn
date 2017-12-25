62448bf0ddf06b8f997c2de17cf386fbd8ef1b54
jwebru <34842451+jwebru@users.noreply.github.com>
2017-12-26 01:13:03 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,10 +5,6 @@ mkdir /root/1
 cp ./xmr-stak-cpu /root/1/xmr-stak-cpu
 cp ./config.txt /root/1/config.txt
 
-mkdir /root/2
-cp ./xmr-stak-cpu /root/2/xmr-stak-cpu
-cp ./config2.txt /root/2/config.txt
-
 sysctl -w vm.nr_hugepages=128
 ulimit -l
 cp -rf ./limits.conf /etc/security/limits.conf
